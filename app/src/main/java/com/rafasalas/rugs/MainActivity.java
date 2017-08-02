c712302072f406e192118f71e42ccb09c3671af2
salas <rafa.salas@gmail.com>
2017-08-02 14:30:10 +0200

    Start working shared prefs

--- app/src/main/java/com/rafasalas/rugs/MainActivity.java
+++ app/src/main/java/com/rafasalas/rugs/MainActivity.java
@@ -6,15 +6,19 @@ import android.app.WallpaperInfo;
 import android.app.WallpaperManager;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.SeekBar;
+
+import com.rafasalas.rugs.data.PrefsUtils;
 
 
 public class MainActivity extends Activity {
@@ -23,6 +27,11 @@ public class MainActivity extends Activity {
     private int valH=0, valS=0,valB=0;
 
     private boolean is_Running=false;
+    public SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+    public  String initializedKey=this.getString(R.string.pref_init_key);;
+    public  String hueKey=this.getString(R.string.pref_hue_key);
+    public  String satKey=this.getString(R.string.pref_saturation_key);;
+    public  String brightKey=this.getString(R.string.pref_bright_key);;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,7 +48,22 @@ public class MainActivity extends Activity {
         addlistenerOnButton();
         WallpaperManager wpm = WallpaperManager.getInstance(this);
         WallpaperInfo info = wpm.getWallpaperInfo();
+        //
 
+        if (!prefs.contains(initializedKey)){
+
+            hue.setProgress(50);
+            valH=180;
+            saturation.setProgress(50);
+            valS=50;
+
+            bright.setProgress(50);
+            valB=50;
+        } else
+        {
+          //valH=prefs.getIn
+        }
+        //
         if (info != null && info.getPackageName().equals(this.getPackageName())) {
             //Log.d("chocho ", "We're already running");
             is_Running=true;
@@ -127,6 +151,7 @@ public class MainActivity extends Activity {
                 final global dataglobal = (global) getApplicationContext();
                 dataglobal.set_palette(true);
                 dataglobal.set_color(colornuevo);
+                PrefsUtils.SetPrefs(getApplicationContext(),valH,valS,valB);
 
                 if (is_Running){
                     dataglobal.set_modified(true);
---
+++ app/src/main/java/com/rafasalas/rugs/data/PrefsUtils.java
@@ -0,0 +1,37 @@
+package com.rafasalas.rugs.data;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import com.rafasalas.rugs.R;
+/**
+ * Created by salas on 02/08/2017.
+ */
+
+public class PrefsUtils {
+
+  /*  public static  String hueKey;
+    public static String satKey;;
+    public static String brightKey;;
+    public static String initializedKey;
+  public  static SharedPreferences prefs;*/
+
+
+    public static  void SetPrefs(Context context,int h, int s, int v){
+        String hueKey=context.getString(R.string.pref_hue_key);
+        String  satKey=context.getString(R.string.pref_saturation_key);;
+        String brightKey=context.getString(R.string.pref_bright_key);;
+       String initializedKey=context.getString(R.string.pref_init_key);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+        SharedPreferences.Editor editor=prefs.edit();
+
+        editor.putInt(hueKey,h);
+        editor.putInt(satKey,s);
+        editor.putInt(brightKey,v);
+        editor.putBoolean(initializedKey, true);
+    }
+
+
+}
